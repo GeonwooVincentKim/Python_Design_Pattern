80c21b42978e7296fffa9812bb5b991af6d0ea6c
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-18 22:06:16 +0900

    Adding requirement Function and Class.

--- Main/Abstract_Factory.py
+++ Main/Abstract_Factory.py
@@ -3,11 +3,19 @@ font-family="sans-sefit" font-size"{fontsize}">{text}</text>"""
 
 SVG_SCALE = 20
 
+BLANK = " "
+CORNER = "+"
+HORIZONTAL = "-"
+VERTICAL = "|"
+
 
 def create_diagram(factory):
     diagram = factory.make_diagram(30, 7)
     rectangle = factory.make_rectangle(4, 1, 2, 5, "yellow")
     text = factory.make_text(7, 3, "Abstract Factory")
+    diagram.add(rectangle)
+    diagram.add(text)
+    return diagram
 
 
 class DiagramFactory:
@@ -37,6 +45,15 @@ class Diagram:
             for x, char in enumerate(row):
                 self.diagram[y + component.y][x + component.x] = char
 
+
+class Rectangle:
+    def __init__(self, x, y, width, height, fill, stroke):
+        self.x = x
+        self.y = y
+        self.rows = _create_ractangle(
+            width, height,
+            BLANK if fill == "white" else "%"
+        )
 
 class SvgText:
     def __init__(self, x, y, text, fontsize):
