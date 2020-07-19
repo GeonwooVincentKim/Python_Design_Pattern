6f0c43394b52b6786f2284e80c9897113b017879
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-19 17:20:10 +0900

    Added other required function and Class.

--- Main/Abstract_Factory.py
+++ Main/Abstract_Factory.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import tempfile
+
 SVG_TEXT = """<text x="{x}" y="{y}" text-anchor="left"\
 font-family="sans-sefit" font-size"{fontsize}">{text}</text>"""
 
@@ -37,33 +41,74 @@ class DiagramFactory:
     def make_rectangle(self, x, y, width, height, fill="white", stroke="black"):
         return Rectangle(x, y, width, height, fill, stroke)
 
-    def make_test(self, x, y, text, fontsize=12):
+    def make_text(self, x, y, text, fontsize=12):
         return Text(x, y, text, fontsize)
 
 
 class Diagram:
-    def __init__(self):
-        self.diagram = None
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.diagram = _create_rectangle(self.width, self.height, BLANK)
 
     def add(self, component):
         for y, row in enumerate(component.rows):
             for x, char in enumerate(row):
                 self.diagram[y + component.y][x + component.x] = char
 
+    def save(self, filenameOrFile):
+        file = None if isinstance(filenameOrFile, str) else filenameOrFile
+        try:
+            if file is None:
+                file = open(filenameOrFile, "w", encoding="utf-8")
+            for row in self.diagram:
+                print("".join(row), file=file)
+        finally:
+            if isinstance(filenameOrFile, str) and file is not None:
+                file.close()
+
 
 class Rectangle:
     def __init__(self, x, y, width, height, fill, stroke):
         self.x = x
         self.y = y
-        self.rows = _create_ractangle(
+        self.rows = _create_rectangle(
             width, height,
             BLANK if fill == "white" else "%"
         )
 
 
+def _create_rectangle(width, height, fill):
+    rows = [[fill for _ in range(width) for _ in range(height)]]
+    for x in range(1, width - 1):
+        rows[0][x] = HORIZONTAL
+        rows[height - 1][x] = HORIZONTAL
+    for y in range(1, height - 1):
+        rows[y][0] = VERTICAL
+        rows[y][width - 1] = VERTICAL
+    for y, x, in((0, 0), (0, width - 1), (height - 1, 0),
+                 (height - 1, width - 1)):
+        rows[y][x] = CORNER
+    return rows
+
+
+"""
+    The differences between DiagramFactory and make_diagram is
+    DiagramFactory.make_diagram) Method returns Diagram Objects,
+    but svgDiagramFactory.make_diagram() Method only returns 
+    SvgDiagram() Objects.
+"""
+
+
 class SvgDiagramFactory(DiagramFactory):
     def make_diagram(self, width, height):
         return SvgDiagram(width, height)
+
+    def make_rectangle(self, x, y, width, height, fill="white", stroke="black"):
+        return SvgRectangle(x, y, width, height, fill, stroke)
+
+    def make_text(self, x, y, text, fontsize=12):
+        return SvgText(x, y, text, fontsize)
 
 
 class SvgDiagram:
@@ -71,6 +116,7 @@ class SvgDiagram:
         pxwidth = width * SVG_SCALE
         pxheight = height * SVG_SCALE
         self.diagram = [SVG_START.format(**locals())]
+        outline = SvgRectangle(0, 0, width, height, "lightgreen", "black")
 
     def add(self, component):
         self.diagram.append(component.svg)
@@ -100,6 +146,20 @@ class SvgText:
         self.svg = SVG_TEXT.format(**locals())
 
 
+def Main():
+    # For regression testing
+    if len(sys.argv) > 1 and sys.argv[1] == "-P":
+        create_diagram(DiagramFactory()).save(sys.stdout)
+        create_diagram(SvgDiagramFactory()).save(sys.stdout)
+        return
+
+
 if __name__ == "__main__":
+    Main()
+
+    textFilename = os.path.join(tempfile.gettempdir(), "diagram.txt")
+    svgFilename = os.path.join(tempfile.gettempdir(), "diagram.svg")
+
     txtDiagram = create_diagram(DiagramFactory())
     txtDiagram.save(textFilename)
+    print("wrote", textFilename)
