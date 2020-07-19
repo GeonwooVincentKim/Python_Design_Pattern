ea0947cbdcd799aa60d7f3db2f3f5db0575016a2
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-19 19:30:31 +0900

    Writing files by classify several Classes and functions

--- Main/Abstract_Factory.py
+++ Main/Abstract_Factory.py
@@ -79,14 +79,14 @@ class Rectangle:
 
 
 def _create_rectangle(width, height, fill):
-    rows = [[fill for _ in range(width) for _ in range(height)]]
+    rows = [[fill for _ in range(width)] for _ in range(height)]
     for x in range(1, width - 1):
         rows[0][x] = HORIZONTAL
         rows[height - 1][x] = HORIZONTAL
     for y in range(1, height - 1):
         rows[y][0] = VERTICAL
         rows[y][width - 1] = VERTICAL
-    for y, x, in((0, 0), (0, width - 1), (height - 1, 0),
+    for y, x in ((0, 0), (0, width - 1), (height - 1, 0),
                  (height - 1, width - 1)):
         rows[y][x] = CORNER
     return rows
@@ -117,9 +117,22 @@ class SvgDiagram:
         pxheight = height * SVG_SCALE
         self.diagram = [SVG_START.format(**locals())]
         outline = SvgRectangle(0, 0, width, height, "lightgreen", "black")
+        self.diagram.append(outline.svg)
 
     def add(self, component):
         self.diagram.append(component.svg)
+
+    def save(self, filenameOrFile):
+        file = None if isinstance(filenameOrFile, str) else filenameOrFile
+        try:
+            if file is None:
+                file = open(filenameOrFile, "w", encoding="utf-8")
+            file.write("\n".join(self.diagram))
+            file.write("\n" + SVG_END)
+
+        finally:
+            if isinstance(filenameOrFile, str) and file is not None:
+                file.close()
 
 
 class SvgRectangle:
@@ -142,7 +155,7 @@ class SvgText:
     def __init__(self, x, y, text, fontsize):
         x += SVG_SCALE
         y += SVG_SCALE
-        fontsize += SVG_TEXT.format(**locals())
+        fontsize += SVG_SCALE // 10
         self.svg = SVG_TEXT.format(**locals())
 
 
@@ -157,9 +170,15 @@ def Main():
 if __name__ == "__main__":
     Main()
 
-    textFilename = os.path.join(tempfile.gettempdir(), "diagram.txt")
-    svgFilename = os.path.join(tempfile.gettempdir(), "diagram.svg")
+    textFilename = os.path.join("E:/Python_Design_Pattern/01/Main", "diagram.txt")
+    svgFilename = os.path.join("E:/Python_Design_Pattern/01/Main", "diagram.svg")
+    # textFilename = os.path.join(tempfile.gettempdir(), "diagram.txt")
+    # svgFilename = os.path.join(tempfile.gettempdir(), "diagram.svg")
 
     txtDiagram = create_diagram(DiagramFactory())
     txtDiagram.save(textFilename)
     print("wrote", textFilename)
+
+    svgDiagram = create_diagram(SvgDiagramFactory())
+    svgDiagram.save(svgFilename)
+    print("wrote", svgFilename)
