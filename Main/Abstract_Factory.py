42b2f7bcc3a7f0b1199579184b9c81b9f6160b30
GeonwooVincentKim <kdsnop@gmail.com>
2020-07-19 19:33:38 +0900

    Change DiagramFactory as a class which are already included '@classmethod'.

--- Main/Abstract_Factory.py
+++ Main/Abstract_Factory.py
@@ -35,13 +35,16 @@ def create_diagram(factory):
 
 # Abstract Class named DiagramFactory.
 class DiagramFactory:
-    def make_diagram(self, width, height):
+    @classmethod
+    def make_diagram(cls, width, height):
         return Diagram(width, height)
 
-    def make_rectangle(self, x, y, width, height, fill="white", stroke="black"):
+    @classmethod
+    def make_rectangle(cls, x, y, width, height, fill="white", stroke="black"):
         return Rectangle(x, y, width, height, fill, stroke)
 
-    def make_text(self, x, y, text, fontsize=12):
+    @classmethod
+    def make_text(cls, x, y, text, fontsize=12):
         return Text(x, y, text, fontsize)
 
 
